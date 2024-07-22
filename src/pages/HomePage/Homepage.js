ae79b3de83af3143ffea0bba4c4c523d3e07da30
andreaairplane <andreadrematos@gmail.com>
2024-07-22 13:13:28 -0400

    fixed homepage

--- src/pages/HomePage/Homepage.js
+++ src/pages/HomePage/Homepage.js
@@ -1,6 +1,11 @@
 function HomePage() {
-    <>
-    </>
+    return (
+        <>
+            <div>
+                <p> Something here </p>
+            </div>
+        </>
+    )
 }
 
 export default HomePage;
